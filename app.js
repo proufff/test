d8dd45abfad181c8bf4cea38a25299388729d751
Me <me@domain.com>
2013-02-13 23:55:30 +0100

    test

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 var port = process.env.PORT || 3000;
 
 var http = require('http');
+var sys  = require('sys');
+var fs   = require('fs');
+
+var blacklist = [];
+
+fs.watchFile('./blacklist', function(c,p) { update_blacklist(); });
+
+function update_blacklist() {
+  sys.log("Updating blacklist.");
+  blacklist = fs.readFileSync('./blacklist').split('\n')
+              .filter(function(rx) { return rx.length })
+              .map(function(rx) { return RegExp(rx) });
+}
 
 http.createServer(function(request, response) {
+  for (i in blacklist) {
+    if (blacklist[i].test(request.url)) {
+      sys.log("Denied: " + request.method + " " + request.url);
+      response.end();
+      return;
+    }
+  }
+
+  sys.log(request.connection.remoteAddress + ": " + request.method + " " + request.url);
   var proxy = http.createClient(80, request.headers['host'])
   var proxy_request = proxy.request(request.method, request.url, request.headers);
-  proxy_request.addListener('response', function (proxy_response) {
+  proxy_request.addListener('response', function(proxy_response) {
     proxy_response.addListener('data', function(chunk) {
       response.write(chunk, 'binary');
     });
@@ -15,9 +37,11 @@ http.createServer(function(request, response) {
     response.writeHead(proxy_response.statusCode, proxy_response.headers);
   });
   request.addListener('data', function(chunk) {
-    proxy_request.write(chunk, 'binary');
+    proxy_request.write(chunk, 'binary);
   });
   request.addListener('end', function() {
     proxy_request.end();
   });
 }).listen(port);
+
+update_blacklist();
