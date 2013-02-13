01ca4cd59184bb63c9332bf4a503ba0f11bb5519
Me <me@domain.com>
2013-02-13 23:39:34 +0100

    test

--- app2.js
+++ app2.js
@@ -1,6 +1,8 @@
+var port = process.env.PORT || 3000;
+
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(80, '127.0.0.1');
+}).listen(port, '0.0.0.0');
 console.log('Server running at http://127.0.0.1:80/');
